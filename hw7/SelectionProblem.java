fe520a4865fea04a9c121437c9beabc19381790f
nathanolson32 <91220215+nathanolson32@users.noreply.github.com>
2021-11-08 14:17:29 -0600

    Update SelectionProblem.java

--- hw7/SelectionProblem.java
+++ hw7/SelectionProblem.java
@@ -6,19 +6,11 @@ public class SelectionProblem <E extends Comparable<E>> {
     int k;
     ArrayList<E> input;
 
-    class oneB {
         public static int kthSmallest(Integer[] input, int k) {
             Arrays.sort(input);
             return input[k - 1];
         }
-        public static void main(String[] args) {
-            Integer arr[] = new Integer[] {12, 3, 5, 7, 19};
-            int k = 1_000_000;
-            System.out.print("K'th smallest element is " + kthSmallest(arr, k));
-        }
-    }
-    class sixA {
-        public static int findKthLargest(List<Integer> nums, int k) {
+        public static int findKthLargestA(List<Integer> nums, int k) {
             PriorityQueue<Integer> input = new PriorityQueue<Integer>(k);
             for (int i: nums) {
                 input.offer(i);
@@ -27,14 +19,7 @@ public class SelectionProblem <E extends Comparable<E>> {
             }
             return input.peek();
         }
-        public static void main(String[] args) {
-            List<Integer> ints = Arrays.asList(7, 4, 6, 3, 9, 1);
-            int k = 1_000_000;
-            System.out.println("k'th largest array element is " + findKthLargest(ints, k));
-        }
-    }
-    class sixB {
-        public static int findKthLargest(List<Integer> ints, int k) {
+        public static int findKthLargestB(List<Integer> ints, int k) {
             if (ints == null || ints.size() < k)
                 System.exit(-1);
             PriorityQueue<Integer> input = new PriorityQueue<>(ints.subList(0, k));
@@ -47,9 +32,15 @@ public class SelectionProblem <E extends Comparable<E>> {
             return input.peek();
         }
         public static void main(String[] args) {
-            List<Integer> ints = Arrays.asList(2*500000);
-            int k = 1_000_000;
-            System.out.println("k'th largest array element is " + findKthLargest(ints, k));
+            Integer arr[] = new Integer[] {12, 3, 4, 7, 19};
+            int k = 2;
+            System.out.println("k'th smallest element is " + kthSmallest(arr, k));
+
+            List<Integer> intsA = Arrays.asList(2, 4, 5, 3, 9, 1);
+            System.out.println("k'th largest  element is " + findKthLargestA(intsA, k));
+
+            List<Integer> intsB = Arrays.asList(1,3,6,4,7,2);
+            System.out.println("k'th largest element is " + findKthLargestB(intsB, k));
+
         }
-    }
 }
